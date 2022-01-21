985affd29b549664c60d55d9b3447da21fdc579e
Kliko Atanasov <kliko.atanasov@gmail.com>
2022-01-21 16:25:06 +0200

    initial commit

---
+++ apps/web3-monorepo-test-app/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'web3-monorepo-test-app',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/web3-monorepo-test-app',
+};
